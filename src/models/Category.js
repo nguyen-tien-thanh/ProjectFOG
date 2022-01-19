f4c92f198f83515dfb9ac806a66b515756a6f35d
Nguyễn Tiến Thành <thanhntgcs190601@greenwich.edu.vn>
2022-01-19 16:00:12 +0700

    Create Database - MongoDB

---
+++ src/models/Category.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const Category = new Schema({
+    name: {type: String, minLength: 1, maxLength: 255},
+    description: {type: String, maxLength: 600},
+    image: {type : String, minLength : 1},
+    createdAt: {type: Date, default : Date.Now},
+    updateAt: {type: Date, default : Date.Now}
+});
+
+module.exports = mongoose.model('category', Category);
