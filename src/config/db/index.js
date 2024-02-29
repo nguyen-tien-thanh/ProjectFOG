39d32b9455b544c061ca342ab8ee8f9c9c8f958b
Nguyễn Tiến Thành <nguyentienthanh.tgdd@gmail.com>
2024-02-29 23:43:57 +0700

    Fix version

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,17 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-async function connect(){
-    
-    mongoose.connect("mongodb+srv://dbadmin:dbadmin@cluster0.eyjvc.mongodb.net/DatabaseFOG?retryWrites=true&w=majority",{
-    // mongoose.connect("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-    })
-    mongoose.connection
-    .once('open',()=> console.log('Database FOG has been connected !!!'))
-    .on('error',(error)=>{
-        console.log("Can not connect to Database !!!", error)
-    })
+async function connect() {
+  mongoose.set("strictQuery", true);
+  mongoose.connect(
+    "mongodb+srv://dbadmin:dbadmin@cluster0.eyjvc.mongodb.net/DatabaseFOG?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     }
+  );
+  mongoose.connection
+    .once("open", () => console.log("Database FOG has been connected !!!"))
+    .on("error", (error) => {
+      console.log("Can not connect to Database !!!", error);
+    });
+}
 
-module.exports = { connect }
+module.exports = { connect };
--- src/main.js
+++ src/main.js
@@ -1,81 +1,81 @@
-const express = require('express')
-const morgan = require('morgan')
-const exphbs = require('express-handlebars')
-const path = require('path');
-const methodOverride = require('method-override');
+const express = require("express");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const methodOverride = require("method-override");
 
-const formidable = require('formidable');
-const session = require('express-session');
+const formidable = require("formidable");
+const session = require("express-session");
 
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
-const passportLocalMongoose = require('passport-local-mongoose');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
 
+const hbs = exphbs.create({
+  helpers: require("./ulti/helpers"),
+  extname: ".hbs",
+});
 
-const hbs = exphbs.create({ 
-  helpers:require('./ulti/helpers'),
-  extname: '.hbs'
-})
+const helpers = require("handlebars-helpers")();
 
-const helpers = require('handlebars-helpers')();
+const route = require("./routes");
+const db = require("./config/db");
+const { options } = require("./routes/idea");
 
-const route = require('./routes');
-const db = require('./config/db');
-const { options } = require('./routes/idea');
+const app = express();
 
-const app = express()
-
-
-
-app.use(express.static('src/public')); 
+app.use(express.static("src/public"));
 // app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, 'uploads')));
+app.use(express.static(path.join(__dirname, "uploads")));
 //HTTP Loggers
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 //Connect Database
 db.connect();
 
-
 //Middleware to solve Body Form
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
 
 //Upload file by POST method
-const uploadFolder =  path.join(__dirname, 'uploads/idea');
+const uploadFolder = path.join(__dirname, "uploads/idea");
 //Basic config for upload file
 const form = formidable(options);
-form.options.uploadDir= uploadFolder; // folder to save file
+form.options.uploadDir = uploadFolder; // folder to save file
 form.multiples = true; // multiple files
 form.maxFileSize = 50 * 1024 * 1024; // 5MB file
 
 //=========SIGN UP SIGN IN AUTHORNIZE ==========================//
-app.use(require("express-session")({
-  secret: "keyboard cat",
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  require("express-session")({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-const User = require('./models/User');
+const User = require("./models/User");
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
 //Template engines
-app.engine('hbs', hbs.engine)
-app.set('view engine', 'hbs')
-app.set("views", path.join(__dirname, 'resources/Views')); // cách mình tìm đến file, hệ điều hành window
-// console.log('PATH: ', path.join(__dirname, '.\\resources\\Views')); 
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/Views")); // cách mình tìm đến file, hệ điều hành window
+// console.log('PATH: ', path.join(__dirname, '.\\resources\\Views'));
 
 // Routes init
 route(app);
 
-
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "\nExpress server listening on port http://localhost:%d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
